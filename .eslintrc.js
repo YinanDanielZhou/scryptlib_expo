03757a71533f2ef975b87452b1cea9a9214cdd00
YinanDanielZhou <zhouyinan49@gmail.com>
2025-03-13 14:20:58 -0700

    modified eslintrc.js to switch the environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 module.exports = {
     "env": {
-        "browser": true,
-        "node": true,
-        "es2020": true
+        // "browser": true,
+        "react-native": true,
+        "es2021": true
     },
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     parser: '@typescript-eslint/parser',
